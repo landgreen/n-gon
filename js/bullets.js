126b1ad9be6d643af17d86f723b4221a414b1dac
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-02-26 22:35:29 +0000

    Umm...

--- js/bullets.js
+++ js/bullets.js
@@ -75,7 +75,7 @@ const b = {
       },
       requires: "minigun, shotgun, super balls, foam",
       effect() {
-        b.modBulletSize += 0.20
+        b.modBulletSize += 0.30
       },
       remove() {
         b.modBulletSize = 1;
